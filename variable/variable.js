a77114fada5b0854988eb7c4bedb8003d17a359d
RAHUL RAJ <108966874+RahulRaj1819@users.noreply.github.com>
2024-03-02 10:32:05 +0000

    container conf

---
+++ variable/variable.js
@@ -0,0 +1,26 @@
+const accountId = 14424 //const cannot be change
+let accountEmail = "rahulrajpck@gmail.com"
+var accountPassword =" 181920"
+accountCity = "New Delhi" //by defualt auto declare
+
+//accountId =2 not allowed
+accountEmail ="abc@gmial.com"
+accountPassword ="12334"
+accountCity =" Benglore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountPassword]);
+
+let name ="Rahul Raj"
+let age =20
+let isLoggedIn =false
+
+
+//number =. 2 to power 53
+//bigint
+//boolean
+//undefined 
+//symbol
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
